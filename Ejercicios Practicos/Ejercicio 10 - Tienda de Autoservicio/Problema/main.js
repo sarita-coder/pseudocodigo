9ee864ea87500ea7570ad07b9217de5a2b781807
Sarita Román <sarita.roman.colonio@gmail.com>
2016-05-04 08:54:42 -0500

    Update main.js

--- Ejercicios Practicos/Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
+++ Ejercicios Practicos/Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
@@ -1,9 +1,23 @@
 //Escribe aquí tú código
-function tiendas(){
-	var cant_ciudad=parseInt(prompt("Cantidad de sucursales en ciudad que "))
-	var ciudad=0;
-	var i=0;
-while(a)
+function ventas(){
+	var ciudad=parseInt(prompt("Ingresa el número de sucursales:"));
+	var total=0;
+	var result = "";
+	for(var i=1;i<=ciudad;i++){
+		var porCiudad = 0;
+		var tienda=parseInt(prompt("Ingresa el número de tiendas en la sucursal "+i+":"));
+		for(var j=1;j<=tienda;j++){
+			var empleados=parseInt(prompt("Ingresa el número de empleados que trabajan en la tienda "+j+":"));
+			var suma=0;	
+			for(var k=1;k<=empleados;k++){
+				var vta_Empleado=parseInt(prompt("Ingresa la venta del empleado "+k+" de la tienda "+j+" y sucursal "+i));
+				suma+=vta_Empleado;
+				porCiudad+= suma;
+				total+= suma;
+			}
+			result += "Venta Total: "+total+ ", Venta Total por la "+i+" ciudad: " +porCiudad+", Total de ventas por la tienda "+j+" : "+suma +". \n";
+		}
 	}
-	
+	return result;
 }
+alert("Informe del día: \n"+ventas());
